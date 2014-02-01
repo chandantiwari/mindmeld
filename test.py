f0c9c8eae52dc23e16ed2cc417c68230affbfdf6
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-02-01 22:38:30 +0100

    changes

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import mindmeld
+import unittest
 
-# grant lewi decans 8:11:10:4:7:32:30:26:10:8:
-# 28,154,163,174,181,188,189,209,220,231
-print mindmeld.calculate_lewi_decan([8,11,10,4,7,32,30,26,10,8])
-# 61
-#print mindmeld.calculate_lewi_decan([17,1,19,22,10,11,28,2,16,12])
-
-#res =  mindmeld.calculate('19020608') 
-
+class TestAll(unittest.TestCase):
+    
+    def testlewi(self):
+        res = mindmeld.calculate('19730424')
+        self.assertTrue('22' in res['lewi'])
+        res = mindmeld.get_lewi('19730424')
+        self.assertTrue('22' in res)
+        
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestAll)
+    unittest.TextTestRunner(verbosity=2).run(suite)
