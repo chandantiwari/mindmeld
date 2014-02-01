c13b1577a84ed4dfad4cf82ad44e061ed766f895
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-02-01 23:34:46 +0100

    changes

--- mindmeld.py
+++ mindmeld.py
@@ -1,27 +1,16 @@
 from datetime import datetime
 import pandas as pd
 import numpy as np
+import mapping
 
 lewi = pd.read_csv('./app/lewi.dat',names=['date','lewis'],sep=' ')
 decans = pd.read_csv('./app/decans.dat',names=['date','decans'],sep=' ')
 spiller = pd.read_csv("./app/spiller",names=['from','to','sign'])
 chinese = pd.read_csv("./app/chinese",names=['from','to','sign'])
+planets = ['sun','mo','mer','ven','mar','ju','sat','ur','nep','pl']
+mapping.init()
 
 sun_moon_table = np.array(range(144)).reshape((12,12)) + 1
-planets = ['sun','mo','mer','ven','mar','ju','sat','ur','nep','pl']
-mapping = pd.DataFrame(index=planets,columns=['tick','*','sq','tri','opp'])
-
-mapping.ix['mo','tick'] = {'sun':245,'mer':145,'ven':146,'mar':147,'ju':148,'sa':149,'ur':150,'ne':151,'pl':254}
-mapping.ix['mo','tri'] = {'sun':246,'mer':152,'ven':153,'mar':154,'ju':155,'sa':156,'ur':157,'ne':158,'pl':255}
-mapping.ix['mo','*'] = {'sun':246,'mer':152,'ven':153,'mar':154,'ju':155,'sa':156,'ur':157,'ne':158,'pl':255}
-mapping.ix['mo','sq'] = {'sun':247,'mer':159,'ven':160,'mar':161,'ju':162,'sa':163,'ur':164,'ne':165,'pl':256}
-mapping.ix['mo','opp'] = {'sun':247,'mer':159,'ven':160,'mar':161,'ju':162,'sa':163,'ur':164,'ne':165,'pl':256}
-mapping.ix['ur','tick'] = {'ne':242,'pl':272}
-mapping.ix['ur','tri'] = {'ne':243,'pl':273}
-mapping.ix['ur','*'] = {'ne':243,'pl':273}
-mapping.ix['ur','sq'] = {'ne':244,'pl':274}
-mapping.ix['ur','opp'] = {'ne':244,'pl':274}
-mapping.ix['ur','tick'] = {'mer':166,'ven':167,'mar':168,'ju':169,'sa':170,'ur':171,'ne':172,'pl':251}
 
 def get_lewi(date):
    tmp=np.array(lewi[lewi['date']==int(date)]['lewis'])
