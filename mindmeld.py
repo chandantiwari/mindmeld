e4970027597d2115abc5e85db30b7bbb41deb9f0
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-02-02 20:57:11 +0100

    changes

--- mindmeld.py
+++ mindmeld.py
@@ -76,28 +76,20 @@ def calculate_lewi_decans(decans):
    decans = np.array(decans)
    for planet in planets:
       decan = decans[planets.index(planet)]
-      #print 'planet',planet,'at decan', decan
       relpos = steps + decan; relpos = map(lambda x: x % 36,relpos)
-      #print 'relpos',relpos
       for pos,step_sign in itertools.izip(relpos,step_signs):
          matches = np.array(range(10))[decans == pos]
          pls = np.array(planets)[decans == pos]
          if len(matches)>0:
-            #print matches,pls,step_sign
             for match,p in itertools.izip(matches,pls):
-               #print 'from',planet,'to',p,step_sign,match
                if not pd.isnull(smap.ix[planet,step_sign]) and (p in smap.ix[planet,step_sign]):
-                  #print smap.ix[planet,step_sign][p]
                   res.append(smap.ix[planet,step_sign][p])
 
-   #print 'self matches---'
    for i,dec in enumerate(decans):
       matches = np.array(range(10))[decans==dec]
       if len(matches) > 1:
-         #print 'matches',matches
          for x in matches:
             if i<x:
-               #print planets[i],'matches',planets[x]
                if not pd.isnull(smap.ix[planets[i],'tick']) and (planets[x] in smap.ix[planets[i],'tick']):
                   res.append(smap.ix[planets[i],'tick'][planets[x]])
             
