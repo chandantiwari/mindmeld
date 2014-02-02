76b0d57bae5e7ec86908353aff1e839425f8db60
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-02-02 18:56:51 +0100

    changes

--- mindmeld.py
+++ mindmeld.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 import pandas as pd
 import numpy as np
-import mapping
+import mapping, itertools
 
 lewi = pd.read_csv('./app/lewi.dat',names=['date','lewis'],sep=' ')
 decans = pd.read_csv('./app/decans.dat',names=['date','decans'],sep=' ')
@@ -69,21 +69,26 @@ def calculate_lewi(decans):
    step_signs = ['*', 'sq', 'tri', 'opp', 'tri', 'sq', '*']
    steps = np.array([6,9,12,18,24,27,30])
    decans = np.array(decans)
-   print 'decans',decans
+   print 'decans',decans,'\n'
    for planet in planets:
       decan = decans[planets.index(planet)]
-      print 'planet',planet,'decan', decan
-      shifted = steps + decan
-      shifted = map(lambda x: x % 36,shifted)
-      print 'shifted',shifted
+      print 'planet',planet,'at decan', decan
+      relpos = steps + decan; relpos = map(lambda x: x % 36,relpos)
+      print 'relpos',relpos
+      for pos,step_sign in itertools.izip(relpos,step_signs):
+         matches = np.array(range(10))[decans == pos]
+         pls = np.array(planets)[decans == pos]
+         if len(matches)>0:
+            print matches,pls,step_sign
+         
       
    return res
 
 
 # grant lewi
 # 28,154,163,174,181,188,189,209,220,231
-#print calculate_lewi([8,11,10,4,7,32,30,26,10,8])
+print calculate_lewi([8,11,10,4,7,32,30,26,10,8])
 
 # book sample 61
-#print calculate_lewi_decan([17,1,19,22,10,11,28,2,16,12])
+#print calculate_lewi([17,1,19,22,10,11,28,2,16,12])
 
