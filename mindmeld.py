7e0153903b087f30e46bb3274e112a24327282af
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-02-02 19:43:29 +0100

    changes

--- mindmeld.py
+++ mindmeld.py
@@ -105,12 +105,12 @@ def calculate_lewi(decans):
 
 
 # grant lewi
-#print calculate_lewi([8,11,10,4,7,32,30,26,10,8])
-#print get_lewi('19020608')
-#print 'from book 28,154,163,174,181,188,189,209,220,231'
+print calculate_lewi([8,11,10,4,7,32,30,26,10,8])
+print get_lewi('19020608')
+print 'from book 28,154,163,174,181,188,189,209,220,231'
 
-print calculate_lewi(get_decans('19730424'))
-print get_lewi('19730424')
+#print calculate_lewi(get_decans('19730424'))
+#print get_lewi('19730424')
 
 # book sample 61
 #print calculate_lewi([17,1,19,22,10,11,28,2,16,12])
