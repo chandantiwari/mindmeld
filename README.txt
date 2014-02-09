8d1b80df60a8bc8c01759cec4886f40b2985286c
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-02-09 21:58:40 +0100

    changes

--- README.txt
+++ README.txt
@@ -28,11 +28,30 @@ consuming. The decan information required for this calculation are
 taken from SwissEph package. We wrapped this library with some Java
 code found under jlewi directory. 
 
-All details for all sign, number explanations can be found under
+All details for sign, number explanations can be found under
 doc/details folder. For Spiller sign Taurus explanation for example
 see under doc/details/spiller/Taurus.html
 
-Myers-Briggs Test can be found under doc folder. Its output is not
+A Myers-Briggs Test can be found under doc folder. Its output is not
 interfaced with Python code yet. There is only simple Javascript
 checks performed on the input.
+
+Myers-Briggs and Astrology Connection
+
+I was curious about how scientific astrology could be; so I performed
+a test using machine learning. I scraped celebrity data from
+celebritytypes.com site for their MBTI types. Seperately I downloaded
+celebrity birthdays from another site and generated their Lewi,
+Millman, Chinese, Spiller sign data. I connected the two, so I had a
+data source of ~300 data points which had both scientific and
+astrologic data to play with.
+
+For each data point, I made it held-out test data, I trained a
+classifier using the rest, and testing the success on the held-out
+record. The aim was trying to predict letters of MBTI type, I or E, N
+or S, etc.
+
+The result I obtained was %57 which indicates the presence of a
+pattern. Since the dimensionality of this data is high, the classifier
+would benefit greatly from presence of more data.
 
--- celebpred.py
+++ celebpred.py
@@ -5,15 +5,17 @@ import sklearn as sk
 import numpy as np
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn import svm
 import random
 
 # Classifier that tries to predict celebrity MBTI letter from
 # astrological parameters. We used celebrity data because usually both
 # their MBTI type and bday is known. This classifier would benefit
-# greatly from more data.
+# greatly from more data. The prediction success is already at %57
+# which indicates a connection between science (psychology,
+# Myers-Briggs) and astrology / numerology.
 
-# tried bunch of different classifiers, highest pred rate is 53.
-clf = DecisionTreeClassifier(max_depth=8) # %58,%62
+clf = DecisionTreeClassifier(max_depth=8) # %57.1
 print clf
 
 df = pd.read_csv("./data/celeb_astro_mbti.csv",sep=';')
@@ -22,26 +24,27 @@ total = 0
 predsum = 0
 for idx in df.index:
    print df.ix[idx]['name']
-   letter = random.choice(['I','N','T','P'])
-   X = df.copy()
-   X = X.fillna(0)
-   y = df[letter]*1
-   testrow = X.ix[idx]
-   testres = X.ix[idx][letter]
-   X = X.drop(idx)
-   y = y.drop(idx)
-   cols = ['I','N','T','P','mbti','name','occup','bday','bday2']
-   X = X.drop(cols,axis=1)
-      
-   res=clf.fit(X,y)
-   print clf.score(X,y) 
+   #letter = random.choice(['I','N','T','P'])
+   for letter in ['I','N','T','P']:
+      X = df.copy()
+      X = X.fillna(0)
+      y = df[letter]*1
+      testrow = X.ix[idx]
+      testres = X.ix[idx][letter]
+      X = X.drop(idx)
+      y = y.drop(idx)
+      cols = ['I','N','T','P','mbti','name','occup','bday','bday2']
+      X = X.drop(cols,axis=1)
 
-   testrow2=testrow.drop(cols)
-   total += 1
-   naive = random.choice([0,1])
+      res=clf.fit(X,y)
+      print clf.score(X,y) 
 
-   pred = clf.predict(testrow2)
-   if pred == testres: predsum += 1
-   if naive == testres: naivesum += 1
-   print 'pred',predsum, 'naive', naivesum, 'total', total, naivesum/float(total)*100, predsum/float(total)*100
+      testrow2=testrow.drop(cols)
+      total += 1
+      naive = random.choice([0,1])
+
+      pred = clf.predict(testrow2)
+      if pred == testres: predsum += 1
+      if naive == testres: naivesum += 1
+      print 'pred',predsum, 'naive', naivesum, 'total', total, naivesum/float(total)*100, predsum/float(total)*100
 
