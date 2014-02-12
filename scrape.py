400e6496c33245726ab5df5c0088fb08b20f34af
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-02-12 04:52:49 +0100

    changes

--- scrape.py
+++ scrape.py
@@ -11,7 +11,7 @@ from urllib import FancyURLopener
 class MyOpener(FancyURLopener):
     version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'        
 
-fout = open("./data/myer-briggs2.txt","w")
+fout = open("/tmp/myer-briggs.txt","w")
 for mbti in ['estp','esfp','estj','esfj','entj','entp','enfj','enfp',
              'istp','isfp','istj','isfj','intj','intp','infj','infp']:
     opener = MyOpener()
