2ac6d6c06522bf80f1f5a0ad7c17fc8f9e970f91
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-03-28 21:50:13 +0100

    changes

--- create_keyed_astro.py
+++ create_keyed_astro.py
@@ -3,24 +3,25 @@ from datetime import datetime
 import pandas as pd
 import mindmeld, numpy as np
 
-# now populate all astrological values using results from mindmeld.calculate
 def f(x):
     res = mindmeld.calculate(str(int(x['bday'])))
-    for lew in res['lewi']: x['lewi'+str(lew)] = 1
-    if res['chinese']: x['chinese'] = res['chinese']
-    if res['spiller']: x['spiller'] = res['spiller']
+    for lew in res['lewi']: x['L'+str(lew)] = 1
+    if res['chinese']: x['C'] = res['chinese']
+    if res['spiller']: x['S'] = res['spiller']
     if res['millman']:
-        x['mill0'] = res['millman'][0]
-        x['mill1'] = res['millman'][1]
+        x['M0'] = res['millman'][0]
+        x['M1'] = res['millman'][1]
     return x
 
-df = pd.read_csv("./data/lewi.dat", sep=' ', names=['bday','rest','chinese','spiller','mill0','mill1'])
+df = pd.read_csv("./data/lewi.dat", sep=' ', names=['bday','rest','C','S','M0','M1'])
 df['bday'] = df['bday'].map(lambda x: int(x))
 df = df[df['bday'] > 19400101]
 df = df[:100]
 
-for x in map(lambda x: 'lewi'+str(x),range(278)): df[x] = np.nan
+for x in map(lambda x: 'L'+str(x),range(278)): df[x] = np.nan
 df2 = df.apply(f, axis=1)
 df2 = df2.drop('rest',axis=1)
-#df3, _, _ = one_hot_dataframe(df2,['spiller','chinese'], replace=True)
+from sha import sha
+df2['S'] = df2['S'].map(lambda x: sha(x).hexdigest())
+df2['C'] = df2['C'].map(lambda x: sha(x).hexdigest())
 df2.to_csv("/tmp/out.csv",sep=';',index=None)
