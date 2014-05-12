b0e20dd2d0a543adbc0e445c601918a9e8b328b3
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-05-12 18:35:44 +0200

    changes

--- create_keyed_astro.py
+++ create_keyed_astro.py
@@ -8,23 +8,34 @@ from datetime import datetime
 import pandas as pd, os
 import mindmeld, numpy as np
 
+def one_hot_dataframe(data, cols, replace=False):
+    vec = DictVectorizer()
+    mkdict = lambda row: dict((col, row[col]) for col in cols)
+    vecData = pd.DataFrame(vec.fit_transform(data[cols].to_dict(outtype='records')).toarray())
+    vecData.columns = vec.get_feature_names()
+    vecData.index = data.index
+    if replace is True:
+        data = data.drop(cols, axis=1)
+        data = data.join(vecData)
+    return (data, vecData, vec)
+
 def f(x):
     res = mindmeld.calculate(str(int(x['bday'])))
     for lew in res['lewi']: x['L'+str(lew)] = 1
     if res['chinese']: x['C'] = res['chinese']
     if res['spiller']: x['S'] = res['spiller']
     if res['millman']:
-        x['M0'] = res['millman'][0]
-        x['M1'] = res['millman'][1]
-        x['M2'] = res['millman'][2]
-        x['M3'] = res['millman'][3]
+        x['M0'] = str(res['millman'][0])
+        x['M1'] = str(res['millman'][1])
+        x['M2'] = str(res['millman'][2])
+        x['M3'] = str(res['millman'][3])
     return x
 
 # get readings using birthday
 df = pd.read_csv("./data/lewi.dat", sep=' ', names=['bday','rest','C','S','M0','M1','M2','M3'])
 df['bday'] = df['bday'].map(lambda x: int(x))
 df = df[df['bday'] > 19400101]
-#df = df[:300]
+#df = df[:1000]
 for x in map(lambda x: 'L'+str(x),range(278)): df[x] = np.nan
 df2 = df.apply(f, axis=1)
 df2 = df2.drop('rest',axis=1)
@@ -35,26 +46,19 @@ cs = list(df2['C'].unique())
 print 'S'
 for i in df2.index:
     print df2.ix[i]['bday']
-    df2.ix[i]['S'] = str(ss.index(df2.ix[i]['S']))
+    df2.ix[i,'S'] = str(ss.index(df2.ix[i,'S']))
 print 'C'
 for i in df2.index:
     print df2.ix[i]['bday']
-    df2.ix[i]['C'] = str(cs.index(df2.ix[i]['C']))
+    df2.ix[i,'C'] = str(cs.index(df2.ix[i,'C']))
+
+df2['C'] = df2['C'].map(lambda x: str(x))
+df2['S'] = df2['S'].map(lambda x: str(x))
 df2['M0'] = df2['M0'].map(lambda x: str(x))
 df2['M1'] = df2['M1'].map(lambda x: str(x))
-
-def one_hot_dataframe(data, cols, replace=False):
-    vec = DictVectorizer()
-    mkdict = lambda row: dict((col, row[col]) for col in cols)
-    res = data[cols].apply(mkdict, axis=1)
-    vecData = pd.DataFrame(vec.fit_transform(res).toarray())
-    vecData.columns = vec.get_feature_names()
-    vecData.index = data.index
-    if replace is True:
-        data = data.drop(cols, axis=1)
-        data = data.join(vecData)
-    return (data, vecData, vec)
-
-df3, _, _ = one_hot_dataframe(df2,['S','C','M0','M1','M2','M3'],True)
+df2['M2'] = df2['M2'].map(lambda x: str(x))
+df2['M3'] = df2['M3'].map(lambda x: str(x))
+    
+df3, _, _ = one_hot_dataframe(df2,['S','C','M0','M1','M2','M3'], True)
 
 df3.to_csv("/tmp/out.csv",sep=';',index=None)
