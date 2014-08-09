9e7c0084f5c44d27e4065d5ced09ec4aada05c3c
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-08-10 00:53:55 +0200

    changes

--- celebpred1.py
+++ celebpred1.py
@@ -18,17 +18,16 @@ from sklearn.svm import LinearSVC
 
 df = pd.read_csv("./data/celeb_astro_mbti.csv",sep=';')
 
-cols = ['I','N','T','P','mbti','name','occup','bday','bday2']
+cols = ['I','N','T','P','mbti','name','occup','bday','bday2','Si','Ti','Ne','Fe','Te','Ni','Se','Fi']
 for letter in ['I','N','T','P']:
-   #clf = LogisticRegression(penalty='l1')
+#for letter in ['Si','Ti','Ne','Fe','Te','Ni','Se','Fi']:
+   #clf = LogisticRegression(penalty='l2')
    clf = LinearSVC()
-
-   print letter
    X = df.copy()
-   X = X.drop(cols,axis=1)
    X = X.fillna(0)
+   y = X[letter]
+   X = X.drop(cols,axis=1)
    Xs = sps.csr_matrix(X)
-   y = df[letter]
    a_train, a_test, y_train, y_test = train_test_split(Xs, y, test_size=0.40,random_state=42)
    clf.fit(a_train, y_train)
    #y_pred = clf.predict_proba(a_test)[:,1]
