2cca33a88cca5b5e5aa456df328bc07f7d99e4ce
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-08-24 07:54:58 +0200

    changes

--- celebpred.py
+++ celebpred.py
@@ -28,8 +28,9 @@ def test():
    clfs = {}
    conf = {}
    for letter in ['Si','Ti','Ne','Fe','Te','Ni','Se','Fi']:
-      clf = LogisticRegression(penalty='l2')
+      #clf = LogisticRegression(penalty='l2')
       #clf = LinearSVC()
+      clf = BernoulliRBM()
       X = df.copy()
       X = X.fillna(0)
       y = X[letter]
@@ -38,8 +39,8 @@ def test():
       Xs = X
       a_train, a_test, y_train, y_test = train_test_split(Xs, y, test_size=0.02,random_state=42) 
       clf.fit(a_train, y_train)
-      y_pred = clf.predict_proba(a_test)[:,1]      
-      #y_pred = clf.predict(a_test)
+      #y_pred = clf.predict_proba(a_test)[:,1]      
+      y_pred = clf.score_samples(a_test)
       fpr, tpr, thresholds = roc_curve(y_test, y_pred)
       roc_auc = auc(fpr, tpr)
       print("%s AUC : %f" % (letter, roc_auc))
