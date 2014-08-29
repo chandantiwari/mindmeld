4a31b33f4a1cf68d3299df92a9d52c8723aff42a
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-08-29 14:56:50 +0200

    changes

--- pred.py
+++ pred.py
@@ -2,7 +2,6 @@ import pandas as pd, celebpred
 import numpy as np
 
 clfs, ocols, conf = celebpred.test()
-print ocols
 df = pd.read_csv("./data/celeb_astro_mbti.csv",sep=';')
 
 print 'Avg AUC', np.array(list(conf.values())).mean()
