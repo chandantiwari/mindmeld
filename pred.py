5114b9ece72a6a7177d6e4ca5e0867d082ec4581
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-08-30 07:49:32 +0200

    changes

--- pred.py
+++ pred.py
@@ -1,5 +1,5 @@
 import pandas as pd, celebpred
-import numpy as np
+import numpy as np, pickle
 
 clfs, train_cols, conf = celebpred.test()
 df = pd.read_csv("./data/celeb_astro_mbti.csv",sep=';')
@@ -11,6 +11,7 @@ df_t.loc[0,'mbti'] = 'xxx'
 df_t.loc[0,'bday'] = '24/04/1973'
 #df_t.loc[0,'bday'] = '22/2/1949'
 #df_t.loc[0,'bday'] = '5/10/1945'
+#df_t.loc[0,'bday'] = '8/1/1982'
 
 df2_t = mineprep.astro_enrich(df_t)
 
@@ -23,10 +24,26 @@ df2_t = df2_t[train_cols]
 XX = np.array(df2_t.fillna(0))
 print XX.shape
 
+a = []
 for clf in clfs: 
     res = clfs[clf].predict_proba(XX)[0] # for Logit
     #res = clfs[clf].score_samples(XX)
     #res = clfs[clf].predict(XX)
     print clf, res, res*conf[clf]
-    
+    a.append( (clf, res, res*conf[clf]) )
+#pickle.dump(a, open( '/tmp/out.pkl', "wb" ) )    
+
+opp = { 'NeFi': 'STJ',  'NeTi': 'SFJ',  'NiTe': 'SFP', 'NiFe': 'STP', 'SiTe': 'NFP',  'SiFe': 'NTP',  'SeFi': 'NTJ',  'SeTi': 'NFJ'}
+map = {'NeFi': 'NFP', 'NeTi': 'NTP', 'NiTe': 'NTJ', 'NiFe': 'NFJ',  'SiTe': 'STJ', 'SiFe': 'SFJ', 'SeFi': 'SFP', 'SeTi': 'STP'}
+
+res = []
+for (mbti,neg,pos) in a: 
+   if len(mbti) == 4: 
+       res.append([map[mbti], opp[mbti], neg[1], pos[1]] )
+df = pd.DataFrame(res, columns=['mbti','opp','neg','pos'])
+
+print df.sort_index(by='pos',ascending=False).head(1)['mbti']
+print df.sort_index(by='neg',ascending=True).head(3)['opp']
+
+
 print df_t.loc[0,'bday']   
