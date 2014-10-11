e24bd3af64d5b9fb1135418aa220dd063a5b4c6b
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-10-11 09:32:38 +0200

    changes

--- train.py
+++ train.py
@@ -2,10 +2,9 @@ import pandas as pd, numpy as np, pickle
 from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.linear_model import Lasso, LinearRegression
+from sklearn.linear_model import Lasso, Ridge, LinearRegression
 
-s = 0.05
-depth = 4
+s = 0.10
 
 letter_cols = ['Si','Ti','Ne','Fe','Te','Ni','Se','Fi']
 junk_cols = ['mbti','name','occup','bday','bday2']
@@ -24,22 +23,30 @@ def train():
 
    #x_train, x_test, y_train, y_test = train_test_split(Xs, y, test_size=s, random_state=55)
    x_train, x_test, y_train, y_test = train_test_split(Xs, y, test_size=s)
-   
-   #clf = RandomForestRegressor(max_depth=depth,n_estimators=2,random_state=42)
-   clf = DecisionTreeRegressor(max_depth=depth)
+
+   train_ys = pd.DataFrame(y_train, columns=letter_cols)
+   top = train_ys.sum().order(ascending=False).head(4).index
+      
+   #clf = RandomForestRegressor(max_depth=3,n_estimators=3)
+   clf = DecisionTreeRegressor(max_depth=4)
    #clf = Lasso()
-   #clf = LinearRegression()
+   #clf = Ridge()
+   
    clf.fit(x_train,y_train)
    res = clf.predict(x_test)
-   hit_arr = []
+   pred_arr = []
+   top_arr = []
 
    for i in range(len(x_test)):
       pred = pd.Series(res[i, :], index=letter_cols).order(ascending=False).head(4).index
       real = pd.Series(y_test[i, :], index=letter_cols).order(ascending=False).head(2).index
       hits = len([x for x in real if x in pred]) / float(len(real))
       print list(pred), list(real), hits
-      hit_arr.append(hits)
-   print 'pred',np.mean(np.array(hit_arr))
+      pred_arr.append(hits)
+      hits = len([x for x in real if x in top]) / float(len(real))
+      top_arr.append(hits)
+   print 'naive',np.mean(np.array(top_arr))
+   print 'pred',np.mean(np.array(pred_arr))
 
    # display most important features
    if 'RandomForest' in str(type(clf)) or 'DecisionTree' in str(type(clf)): 
@@ -49,7 +56,7 @@ def train():
       print np.array(imps.index)
    if 'DecisionTree' in str(type(clf)):
       import disp
-      disp.get_lineage(clf, df2.columns)
+      #disp.get_lineage(clf, df2.columns)
       
    pickle.dump(clf, open( './data/train.pkl', "wb" ) )
 
