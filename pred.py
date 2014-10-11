44ce4fca958c47da13909ae03c50c3ef15090090
skorsky12 <skorsky@burak-Aspire-V5-122P.(none)>
2014-10-11 07:59:43 +0200

    changes

--- pred.py
+++ pred.py
@@ -1,5 +1,5 @@
 import pandas as pd, mineprep, pickle
-import numpy as np, celebpred_forest
+import numpy as np, train
 
 df = pd.read_csv("./data/celeb_astro_mbti.csv",sep=';')
 clf = pickle.load(open('./data/forest.pkl', 'rb'))
@@ -12,11 +12,11 @@ def pred_mbti(x):
     for col in df.columns:
         if col not in df2_t.columns: df2_t[col] = np.nan
     df2_t = df2_t[df.columns]
-    df2_t = df2_t.drop(celebpred_forest.junk_cols + celebpred_forest.letter_cols, axis=1)
+    df2_t = df2_t.drop(train.junk_cols + train.letter_cols, axis=1)
     df2_t = df2_t.fillna(0)
     #print list(np.array(df2_t))
     res = clf.predict(df2_t)
-    pred = pd.Series(np.ravel(res), index=celebpred_forest.letter_cols)
+    pred = pd.Series(np.ravel(res), index=train.letter_cols)
     pred = pred.order(ascending=False).head(4).index    
     print df_t.loc[0,'bday']
     print pred
